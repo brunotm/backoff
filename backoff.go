0ba00afc208b5d3e98f150f52a344557fc221eaf
Bruno Moura <brunotm@gmail.com>
2018-07-07 16:15:37 +0100

    Use context for cancellation
    Retry now takes a context.Context for cancellation
    Add Until to retry indefinitely
    Fix overflow when running a large number of retries

--- backoff.go
+++ backoff.go
@@ -1,41 +1,20 @@
-/*
-Package backoff provides two simple backoff and jitter implementations for retrying operations
-as described in https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
-
-Example:
-
-package main
-
-import (
-	"fmt"
-	"time"
-
-	"github.com/brunotm/backoff"
-)
-
-func main() {
-	count := 0
-	err := backoff.Retry(
-		5,                      // attempts
-		100*time.Millisecond,   // min
-		3*time.Second,          // max
-		func() error {
-			count++
-			fmt.Println("Count: ", count)
-			if count == 5 {
-				return nil
-			}
-			return fmt.Errorf("op error")
-		})
-	fmt.Println(err)
-}
-
-*/
 package backoff
 
 import (
+	"context"
+	"errors"
+	"math"
 	"math/rand"
 	"time"
+)
+
+var (
+	// ErrContextDone returned when context is canceled
+	ErrContextDone = errors.New("context done")
+)
+
+const (
+	factor = 2
 )
 
 func init() {
@@ -43,41 +22,56 @@ func init() {
 }
 
 // Retry the given function n times jittering between max and min time.Duration
-func Retry(attempts int64, base, max time.Duration, f func() error) (err error) {
-	mx := int64(max)
-	mn := int64(base)
+func Retry(ctx context.Context, attempts int, base, max time.Duration, f func() error) (err error) {
 
-	for attempt := int64(1); attempt <= attempts; attempt++ {
+	for attempt := 1; attempt <= attempts; attempt++ {
 
-		if err = f(); err == nil {
-			break
+		select {
+		case <-ctx.Done():
+			return ErrContextDone
+		default:
 		}
 
-		j := rand.Int63n(min(mx, mn*pow(2, attempts))-mn) + mn
-		time.Sleep(time.Duration(j))
+		if err = f(); err == nil {
+			return nil
+		}
 
+		jitterSleep(attempt, base, max)
 	}
 
 	return err
 }
 
-// pow for int64
-func pow(a, b int64) int64 {
-	var p int64 = 1
-	for b > 0 {
-		if b&1 != 0 {
-			p *= a
+// Until is like retry but retries until success
+func Until(ctx context.Context, base, max time.Duration, f func() error) (err error) {
+
+	for attempt := 1; ; attempt++ {
+		select {
+		case <-ctx.Done():
+			return ErrContextDone
+		default:
 		}
-		b >>= 1
-		a *= a
+
+		if err := f(); err == nil {
+			return nil
+		}
+
+		jitterSleep(attempt, base, max)
+
+		if attempt == math.MaxInt64 {
+			attempt = 1
+		}
 	}
-	return p
 }
 
-// min for int64
-func min(a, b int64) int64 {
-	if a < b {
-		return a
+func jitterSleep(attempt int, base, max time.Duration) {
+	mx := float64(max)
+	mn := float64(base)
+
+	dur := mn * math.Pow(factor, float64(attempt))
+	if dur > mx {
+		dur = mx
 	}
-	return b
+	j := time.Duration(rand.Float64()*(dur-mn) + mn)
+	time.Sleep(j)
 }
